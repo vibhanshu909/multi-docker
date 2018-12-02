77362d91b5db0a271fc2153ca5e7911c8201bdec
vibhanshu <vibhanshu909@gmail.com>
2018-12-02 08:57:23 +0530

    updated to version2

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,12 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h1>Fib Calculator version 2</h1>
             <div>
               <Route exact path='/' component={Fib} />
               <Route exact path='/otherpage' component={OtherPage} />
             </div>
-          </header>
+          </header>9999
         </div>
       </Router>
     );
